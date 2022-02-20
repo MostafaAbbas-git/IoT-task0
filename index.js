f95f7c8ad8984ff1d70e0878d4486a37395ff7d5
Mostafa Mahmoud <Mostafa.AbdElSadek99@eng-st.cu.edu.eg>
2022-02-20 02:11:24 +0200

    temp in index

--- index.js
+++ index.js
@@ -16,6 +16,42 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/home.html");
 });
 
+/////////////////////
+
+app.get("/read", async (req, res) => {
+    // Retrieve all readings coming from sensor1 from the database as an object
+    const readings = await Sensor1.query().orderBy('id').select('readings');
+
+    // extract only the values from the object
+    let values_array = [];
+    for (let i = 0; i < readings.length; i++) {
+        values_array[i] = readings[i].readings;
+    }
+
+    // construct a json with the total number of readings and the values then send it
+    res.status(200).json({
+        totalValues: readings.length,
+        Values: values_array,
+    });
+
+});
+
+
+router.post("/update", async (req, res) => {
+    // insert into the database the temperature value
+    const value = await Sensor1.query().insert({
+        readings: req.body.temperature
+    });
+
+    // send the newly added value 
+    res.status(200).json({
+        message: "Value saved",
+        value: value.readings,
+    });
+});
+
+
+///////////////////
 // PORT
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
