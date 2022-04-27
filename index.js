224ef15e8442222a982f66bff67033096ff6516f
Mostafa Mahmoud <61292945+MostafaAbbas-git@users.noreply.github.com>
2022-04-27 07:47:31 +0200

    <YOUR COMMIT MESSAGE>

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const winston = require('winston');
 const server = require('http').createServer(app);
+const cors = require("cors");
 
 require('./startup/routes')(app);
 require("./startup/database")();
 require("./startup/logging");
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 
+app.use(cors(corsOptions)) // Use this after the variable declaration
 /* HomePage */
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/views/home.html");
--- routes/readings.js
+++ routes/readings.js
@@ -93,6 +93,20 @@ router.delete("/", cors(), async (req, res) => {
     });
 });
 
+router.post("/button", cors(), async (req, res) => {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+
+    // send the response as json with the last read of each sensor
+    res.status(200).json({
+        message: "State saved",
+        buttonState: req.body.buttonState
+    });
+});
 module.exports = router;
 
 /* 
